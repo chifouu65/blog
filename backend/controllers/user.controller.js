f0a49b50f4b693c2908df8c68bd437a97936aa71
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-11-23 15:10:48 +0100

    .

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,40 +13,42 @@ exports.signup = (req, res, next) => {
                 password: hash
             });
             user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-                .catch(error => res.status(400).json({ error }));
+                .then(() => res.status(201).json({message: 'Utilisateur créé !'}))
+                .catch(error => res.status(400).json({error}));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({error}));
 }
 
 exports.login = (req, res, next) => {
-    Model.findOne({ email: req.body.email })
+    Model.findOne({email: req.body.email})
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                return res.status(401).json({error: 'Utilisateur non trouvé !'});
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                        return res.status(401).json({error: 'Mot de passe incorrect !'});
                     }
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
-                            { userId: user._id },
-                            process.env.JWT_TOKEN,
-                            { expiresIn: '24h' }
+                            {userId: user._id},
+                            process.env.TOKEN,
+                            {expiresIn: '24h'}
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => {
+                    res.status(500).json({error})
+                });
         })
-        .catch(error => res.status(500).json({ message: 'Utilisateur non trouvé !' }));
+        .catch(error => res.status(500).json({error}));
 }
 
 exports.deleteUser = (req, res, next) => {
-    Model.deleteOne({ _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
-        .catch(error => res.status(400).json({ error }));
+    Model.deleteOne({_id: req.params.id})
+        .then(() => res.status(200).json({message: 'Utilisateur supprimé !'}))
+        .catch(error => res.status(400).json({error}));
 }
 
