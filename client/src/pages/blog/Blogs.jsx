b256a0c9d93eaf32ba73044aab259ccb950b93e9
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-11-25 18:39:24 +0100

    .

--- client/src/pages/blog/Blogs.jsx
+++ client/src/pages/blog/Blogs.jsx
@@ -2,52 +2,54 @@ import axios from "axios";
 import {Link} from "react-router-dom";
 import React, {useState, useEffect} from "react";
 import styled from 'styled-components'
+
 const Bar = styled.div`
-min-height: 1px;
-width: 100%;
-background-color: rgba(0,0,0,0.5);
-content: "";
-margin: 10px auto;
+  min-height: 1px;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  content: "";
+  margin: 10px auto;
 `
 const BlogPage = styled.section`
-display: flex;
-flex-direction: column;
-padding: 0 20px;
-padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+  margin-top: 20px;
 `
 const BlogContainer = styled.ul`
-display: flex;
-grid: 1fr 1fr 1fr;
-grid-gap: 1rem;
-margin-top: 20px;
-flex-wrap: wrap;
-width: 100%;
+  display: flex;
+  gap: 1rem;
+  margin-top: 20px;
+  flex-wrap: wrap;
+  flex-direction: column-reverse;
+  width: 100%;
 `
 const BlogCard = styled.div`
-background-color: #f1f1f1;
-border-radius: 15px;
-padding: 20px;
-width:100%;
-a > div {
+  background-color: #f1f1f1;
+  border-radius: 15px;
+  padding: 20px;
+
+  a > div {
     display: flex;
     flex-direction: column;
     gap: 5px;
-}
+  }
 `
 const BlogText = styled.div`
-h2 {
+  h2 {
     text-transform: uppercase;
-}
-h3 {
-    display:flex;
+  }
+
+  h3 {
+    display: flex;
     flex-direction: row;
     justify-content: space-between;
     gap: 20px;
-}
+  }
 `
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
-    const [user , setUser] = useState(JSON.parse(localStorage.getItem("user")))
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")))
     useEffect(() => {
         axios.get("http://localhost:5000/posts")
             .then(res => {
@@ -60,24 +62,24 @@ const Blogs = () => {
     return (
         <BlogPage>
             <h1>Blogs</h1>
-            { user ? 
-            <Link to='/post'>Create post</Link> : null
+            {user ?
+                <Link to='/post'>Create post</Link> : null
             }
             <BlogContainer>
-            {blogs.map(blog => (
-                <BlogCard key={blog._id}>
-                    <Link to={`/blog/${blog._id}`}>
-                        <BlogText>
-                        <h2>{blog.title}</h2>
-                         <Bar/>
-                        <p>{blog.desc}</p>
-                         <Bar/>
-                        <h3><span>{blog.username}</span><span>date en atyeyye</span></h3>
-                        </BlogText>
-                    </Link>
-                </BlogCard>
-            ))}
-          </BlogContainer>
+                {blogs.map(blog => (
+                    <BlogCard key={blog._id}>
+                        <Link to={`/blog/${blog._id}`}>
+                            <BlogText>
+                                <h2>{blog.title}</h2>
+                                <Bar/>
+                                <p>{blog.desc}</p>
+                                <Bar/>
+                                <h3><span>{blog.username}</span><span>date en atyeyye</span></h3>
+                            </BlogText>
+                        </Link>
+                    </BlogCard>
+                ))}
+            </BlogContainer>
         </BlogPage>
     );
 }
--- client/src/pages/user/Post.jsx
+++ client/src/pages/user/Post.jsx
@@ -1,26 +1,86 @@
 import axios from "axios";
 import React from "react";
+import styled from "styled-components";
 
-const Post = () => {
-    const [title, setTitle] = React.useState("")
-    const [description, setDescription] = React.useState("")
-    const [username, setUsername] = React.useState("")
+const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 20px;
+    margin: 20px;
+    `
+const Input = styled.input`
+    width: 100%;
+    height: 40px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 0 10px;
+    margin: 10px 0;
+    `
+const Button = styled.button`
+    width: 100%;
+    height: 40px;
+    border: none;
+    border-radius: 5px;
+    background-color: #333;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    &:hover {
+        background-color: #555;
+    }
+    `
+
+const Post =  () => {
+    const auth = localStorage.getItem('user');
+    const user = JSON.parse(auth);
+    let [title, setTitle] = React.useState("")
+    let [desc, setDescription] = React.useState("")
+    let [username, setUsername] = React.useState(user.email)
+    let [category, setCategory] = React.useState("")
+
     const handleChange = (e) => {
         e.preventDefault()
         const Post = {
             title,
-            description,
+            desc,
             username,
+            category,
         }
-        axios.post('http://localhost:5000/post', Post)
+        axios.post('http://localhost:5000/posts', Post)
             .then(res => console.log(res.data))
+            .then(alert('Post Created'))
             .catch(err => console.log(err))
     }
     return (
         <>
-        
+            <h1>POST</h1>
+            <Form>
+                <label htmlFor="title">Title</label>
+                <Input type="text" name="title" id="title" onChange={(e) => setTitle(e.target.value)}/>
+
+                <label htmlFor="desc">Description</label>
+                <Input type="text" name="desc" id="desc" onChange={(e) => setDescription(e.target.value)}/>
+
+
+                <label htmlFor="username">UserName : {username}</label>
+                <Input/>
+
+                <label htmlFor="category">Category</label>
+                <Input type="text" name="category" id="category" onChange={(e) => setCategory(e.target.value)}/>
+
+                <Button onClick={handleChange} type="submit">Post</Button>
+            </Form>
         </>
-     )    
+    )
 }
 
 export default Post;
